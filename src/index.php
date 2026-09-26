027efb99da4b0457da6f0565c70718638df1e599
harshit <rawatjyoti2601@gmail.com>
2026-09-26 19:19:13 +0000

    Test automated trigger on git push

--- src/index.php
+++ src/index.php
@@ -1,14 +1,1 @@
-<?php
-$host = 'mysql-db';
-$db   = 'test_db';
-$user = 'root';
-$pass = 'rootpassword';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-try {
-     $pdo = new PDO($dsn, $user, $pass);
-     echo "Database connection successful!";
-} catch (\PDOException $e) {
-     echo "Connection failed: " . $e->getMessage();
-}
-?>
+<?php echo '<h1>CI/CD Deployment Successful</h1>'; ?>
